ffc667d49673e7987802ad2a9e41dcdc466963be
Ary <ikomangaryantara382@gmail.com>
2024-02-04 12:42:18 +0800

    create (Expenses) : main file for expenses feature

---
+++ Expense/index.js
@@ -0,0 +1,9 @@
+import { Template } from "./Lib/Template";
+import { TRANSACTIONS } from "./global";
+import { Draw } from "./Lib/Draw";
+
+const draw = new Draw
+
+export {
+    Template, TRANSACTIONS, draw
+}
